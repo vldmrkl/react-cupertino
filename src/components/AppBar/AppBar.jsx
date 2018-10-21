6b012ef9d78c3f5c8803d44c9f3715ad6dcf2348
Volodymyr <klymenkodp@gmail.com>
2018-10-21 17:51:31 -0400

    Created appbar template

---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './AppBar.css';
+class AppBar extends React.Component{
+
+    
+    render(){
+        return(
+            <header className="cupertino-app-bar">
+                <div className="bar-content">
+                Test
+                </div>
+                <div className="bar-separator">
+
+                </div>
+            </header>
+        );
+    }
+}
+
+export default AppBar;
