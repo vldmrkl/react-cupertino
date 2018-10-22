badfba052e5c6496057ae3fa49db29d0201652ce
Volodymyr <klymenkodp@gmail.com>
2018-10-22 00:29:09 -0400

    Added logo + links

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import './AppBar.css';
 class AppBar extends React.Component{
-
+    // constructor(props){
+    //     super(props);
+    // }
     
     render(){
-        return(
-            <header className="cupertino-app-bar">
-                <div className="bar-content">
-                Test
-                </div>
-                <div className="bar-separator">
+        let position = "";
+        if(this.props.fixed){
+            position = "fixed";
+        } else{
+            position = "static";
+        }
 
+        return(
+            <header className="cupertino-app-bar" style={{position: position}}>
+                <div className="bar-content">
+                    {this.props.logo &&
+                        <img className="app-bar-logo" src={this.props.logo} alt="logo" />
+                    }
+                    <div className="bar-links">
+                        <ul>
+                            {this.props.children}
+                        </ul>
+                    </div>
                 </div>
+                <div className="bar-separator"></div>
             </header>
         );
     }
