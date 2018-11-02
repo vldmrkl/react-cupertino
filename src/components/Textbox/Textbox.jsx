ca390bcd936a22ca9b60cd656c0c368d05d54705
Volodymyr <klymenkodp@gmail.com>
2018-11-01 22:48:01 -0400

    Add handleChange to update state with current value

--- src/components/Textbox/Textbox.jsx
+++ src/components/Textbox/Textbox.jsx
@@ -2,61 +2,70 @@ import React from 'react';
 import './Textbox.css';
 
 class Textbox extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             value: '',
             invalid: false,
             focused: false
         };
+        this.handleChange = this.handleChange.bind(this);
         this.handleFocus = this.handleFocus.bind(this);
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.setState({
             invalid: this.props.invalid
         })
     }
 
-    componentWillReceiveProps(newProps){
-        if(this.props.invalid !== newProps.invalid){
+    componentWillReceiveProps(newProps) {
+        if (this.props.invalid !== newProps.invalid) {
             this.setState({
                 invalid: newProps.invalid
             })
         }
     }
 
-    handleFocus(){
+    handleChange(e) {
+        this.setState({
+            value: e.target.value
+        });
+    }
+
+    handleFocus() {
         this.setState({
             focused: !this.state.focused
         });
     }
 
-    render(){
+    render() {
         let extraStyle = {};
         let extraStyleForLabel = {};
-        if (this.state.invalid){
-            if(this.state.focused){
-                extraStyle = {"border": "1px solid gray", "backgroundColor": "white"};
-            } else{
-            extraStyle = {"border": "1px solid #FF2968", "backgroundColor": "#fcc2c7"};
+        if (this.state.invalid) {
+            if (this.state.focused) {
+                extraStyle = { "border": "1px solid gray", "backgroundColor": "white" };
+            } else {
+                extraStyle = { "border": "1px solid #FF2968", "backgroundColor": "#fcc2c7" };
             }
-            extraStyleForLabel = {"color": "#FF2968", "opacity": 1}
-        } else{
+            extraStyleForLabel = { "color": "#FF2968", "opacity": 1 }
+        } else {
             extraStyle = {};
             extraStyleForLabel = {};
         }
-        
+
         let type = "text";
-        if(this.props.password){
+        if (this.props.password) {
             type = "password"
         }
-        return(
+        return (
             <div className="tb-div">
-                <input type={type} className="cupertino-textbox" onFocus={this.handleFocus} 
-                                    onBlur={this.handleFocus} 
-                                    style={extraStyle} 
-                                    required />
+                <input type={type} className="cupertino-textbox"
+                    onChange={this.handleChange}
+                    onBlur={this.handleFocus}
+                    onFocus={this.handleFocus}
+                    style={extraStyle}
+                    required />
                 <span className="rs-float-label" style={extraStyleForLabel}>{this.props.label}</span>
             </div>
         );
