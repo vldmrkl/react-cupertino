d691d9877dea150d2b9b9a487f11b1a9a13d62c0
Volodymyr <klymenkodp@gmail.com>
2018-11-26 00:21:15 -0500

    Add stories to test props

---
+++ src/stories/Slider.stories.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Slider from '../components/Slider';
+
+storiesOf('Slider', module)
+    .add('with startValue props', () =>
+        <div style={{width: "15%"}}>
+            <Slider startValue={25} />
+        </div>)
+        .add('with maxValue props', () =>
+        <div style={{width: "15%"}}>
+            <Slider maxValue={500} />
+        </div>)
+        .add('with backgroundColor', () =>
+        <div style={{width: "15%"}}>
+            <Slider backgroundColor="pink" />
+        </div>)
+        .add('with progressColor', () =>
+        <div style={{width: "15%"}}>
+            <Slider progressColor="orange" />
+        </div>);
