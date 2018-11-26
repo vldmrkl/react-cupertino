294b21aef58bcf10530713983d6a4d9292f10172
Volodymyr <klymenkodp@gmail.com>
2018-11-26 00:06:16 -0500

    Add stories to test props

---
+++ src/stories/RadioButton.stories.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import RadioButton from '../components/RadioButton';
+
+storiesOf('RadioButton', module)
+  .add('checked', () => <RadioButton title="Checked" theme="dark" color="red" checked />)
+  .add('with color props', () => <RadioButton title="Purple" theme="dark" color="purple" />)
+  .add('with size props', () => <RadioButton title="Small" theme="dark" size="small" />)
+  .add('with theme props', () => <RadioButton title="Dark" theme="dark" />);
