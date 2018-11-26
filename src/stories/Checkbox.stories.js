1afe26ab5764949e159e513516a815b22496b04d
Volodymyr <klymenkodp@gmail.com>
2018-11-26 00:53:43 -0500

    Add stories to test props

---
+++ src/stories/Checkbox.stories.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Checkbox from '../components/Checkbox';
+
+
+storiesOf('Checkbox', module)
+    .add('checked', () => <Checkbox colorChecked="green" checked />)
+    .add('with colorChecked', () => <Checkbox colorChecked="red" />)
+    .add('with colorUnchecked', () => <Checkbox colorUnchecked="orange" />)
+    .add('with size props', () => <Checkbox size="medium" />);
