6eb030de9560daf2da6b8e48a85f91a3bc78d4f1
Volodymyr <klymenkodp@gmail.com>
2018-12-23 20:39:38 -0500

    Add stories for onChange props

--- src/stories/Slider.stories.js
+++ src/stories/Slider.stories.js
@@ -3,6 +3,10 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Slider from '../components/Slider';
 
+function handleChange(e) {
+  console.log(e.target.value);
+}
+
 storiesOf('Slider', module)
   .add('with startValue props', () => (
     <div style={{ width: '15%' }}>
@@ -22,5 +26,10 @@ storiesOf('Slider', module)
   .add('with progressColor', () => (
     <div style={{ width: '15%' }}>
       <Slider progressColor="orange" />
+    </div>
+  ))
+  .add('with onChange', () => (
+    <div style={{ width: '15%' }}>
+      <Slider onChange={handleChange} />
     </div>
   ));
