d8beaf2cd8ef683495d0d5ed03cf10246fdac775
Volodymyr <klymenkodp@gmail.com>
2018-12-28 18:42:51 -0500

    Add story for checkbox with custom icon

--- src/stories/Checkbox.stories.js
+++ src/stories/Checkbox.stories.js
@@ -2,9 +2,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Checkbox from '../components/Checkbox';
+import icon from './assets/fb.png';
 
 storiesOf('Checkbox', module)
   .add('checked', () => <Checkbox colorChecked="green" checked />)
   .add('with colorChecked', () => <Checkbox colorChecked="red" />)
   .add('with colorUnchecked', () => <Checkbox colorUnchecked="orange" />)
-  .add('with size props', () => <Checkbox size="medium" />);
+  .add('with size props', () => <Checkbox size="medium" />)
+  .add('with icon props', () => <Checkbox icon={icon} size="medium" />);
