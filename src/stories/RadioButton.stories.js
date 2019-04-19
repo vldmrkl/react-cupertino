df1f9cee130f2c29dd8536bdad9826144aae6b73
Volodymyr <klymenkodp@gmail.com>
2019-04-18 21:42:37 -0400

    Add story for a group of radiobuttons

--- src/stories/RadioButton.stories.js
+++ src/stories/RadioButton.stories.js
@@ -7,8 +7,18 @@ storiesOf('RadioButton', module)
   .add('checked', () => (
     <RadioButton title="Checked" theme="dark" color="red" checked />
   ))
+  .add('group', () => (
+    <React.Fragment>
+    <RadioButton title="1" id="1" theme="dark" size="small" />
+    <RadioButton title="2" id="2" theme="dark" size="small" />
+    <RadioButton title="3" id="3" theme="dark" size="small" />
+    </React.Fragment>
+  ))
   .add('with color props', () => (
     <RadioButton title="Purple" theme="dark" color="purple" />
+  ))
+  .add('with onChange props', () => (
+    <RadioButton title="Option 1" theme="dark" onChange={()=>{console.log('clicked')}} />
   ))
   .add('with size props', () => (
     <RadioButton title="Small" theme="dark" size="small" />
